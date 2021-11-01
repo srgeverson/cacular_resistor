ca290bf2fb255bb4947ea3a5c951130c5c55792a
geverson <geversonjosedesouza@gmail.com>
2021-11-01 11:12:27 -0300

    Projeto na versão 8.2 netbeans

---
+++ netbeans_8_2/calcular_resistor/src/calcular_resistor/Calcular_resistor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcular_resistor;
+
+/**
+ *
+ * @author jose icaro
+ */
+public class Calcular_resistor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
